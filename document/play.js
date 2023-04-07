e5b23785f68ce23d1d15473e33e95c3ccf893244
swathivetri <swathivetri1992@gmail.com>
2023-04-07 09:34:44 +0530

    server.req

---
+++ document/play.js
@@ -0,0 +1,13 @@
+ const http=require('http');
+ const server=http.createServer((req,res) => {
+      console.log(req.url, req.method, req.headers);
+      //process.exit();
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html>');
+      res.write('<head><title>My First Page</title><head>');
+      res.write('<body><h1>SwathiRam!</h1></body>');
+      res.write('</html>');
+      res.end();
+ });
+
+ server.listen(4000);
